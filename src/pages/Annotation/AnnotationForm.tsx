18de293ed0cd896f7adee1fba2c7bd5308c72a0a
I570545 <53847614+jay9645@users.noreply.github.com>
2022-11-24 17:43:08 +0800

    Complete input portion

--- src/pages/Annotation/AnnotationForm.tsx
+++ src/pages/Annotation/AnnotationForm.tsx
@@ -2,7 +2,7 @@ import { InfoCircleOutlined } from '@ant-design/icons';
 import { Button, Form, Input, Radio, Typography, Space } from 'antd';
 import React, { useState } from 'react';
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 const { TextArea } = Input;
 
 type RequiredMark = boolean | 'optional';
@@ -15,10 +15,39 @@ const AnnotationForm: React.FC = () => {
   const [annotatedOutput, setAnnotatedOutput] = useState([]);
   const [popoverIsOpen, setPopoverIsOpen] = useState(false);
 
-  const onFill = () => {
+  interface EntityProps {
+    value: string;
+  }
+
+  const EntityButton = (props: EntityProps) => {
+    return <Button>{props.value}</Button>;
+  }
+
+  interface EntityListProps {
+    list: Array<string>
+  }
+
+  const EntityList = (props: EntityListProps) => {
+    const list = props.list;
+    return <>{list.map((entity) =>
+      <EntityButton 
+        key={entity}
+        value={entity} 
+      />
+    )}</>;
+  }
+
+  const onAddText = () => {
     const value = form.getFieldValue("rawText");
-    console.log(value);
+    setText(value);
+    form.setFieldValue("rawText", "");
   };
+
+  const onAddEntity = () => {
+    const value = form.getFieldValue("entity");
+    setEntities(prev => [...prev, value]);
+    form.setFieldValue("entity", "");
+  }
 
   return (
     <Form
@@ -26,17 +55,17 @@ const AnnotationForm: React.FC = () => {
       layout="vertical"
       initialValues={{ rawText: "", entity: "" }}
     >
-      <Title level={3}>Input Entities</Title>
-      <Form.Item label="Raw Text" name="rawText" tooltip="Input the text you would like to annotate here and click on 'Add Raw Text'">
+      <Title level={3}>Input</Title>
+      <Form.Item label="Raw Text" name="rawText" tooltip="Input the text you would like to annotate here and click on 'Add Text'">
         <TextArea rows={4} placeholder="Input raw text here" />
       </Form.Item>
 
       <Form.Item>
         <Space>
           <Button type="primary" htmlType="submit">
-            Submit Text
+            Add Text
           </Button>
-          <Button htmlType="button" onClick={onFill}>
+          <Button htmlType="button" onClick={onAddText}>
             Fill
           </Button>
         </Space>
@@ -44,24 +73,31 @@ const AnnotationForm: React.FC = () => {
 
       <Form.Item
         label="Entity"
-        tooltip="'Input the entity name that you would like to annotate your text with.'"
+        name="entity"
+        tooltip="Input the entity name that you would like to annotate your text with."
       >
         <Input placeholder="Input named entity" />
       </Form.Item>
       <Form.Item>
-        <Button type="primary">Submit</Button>
+        <Button type="primary" onClick={onAddEntity}>Add Named Entity</Button>
       </Form.Item>
 
-      <Form.Item>
-        <Space>
-          <Button type="primary" htmlType="submit">
-            Submit Text
-          </Button>
-          <Button htmlType="button" onClick={onFill}>
-            Fill
-          </Button>
-        </Space>
-      </Form.Item>
+      <Title level={3}>Annotate</Title>
+
+      {entities.length > 0 && <Form.Item>
+        <EntityList list={entities}/>
+        <Button
+          type="ghost"
+          onClick={() => setEntities([])}
+        >
+          Clear Named Entities
+        </Button>
+      </Form.Item>}
+
+      {text.length > 0 &&
+      <Form.Item label="Annotate Text" tooltip="Input the entity name that you would like to annotate your text with.">
+        <Paragraph><pre>{text}</pre></Paragraph>
+      </Form.Item>}
     </Form>
   );
 };
